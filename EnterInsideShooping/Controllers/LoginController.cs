53ea8bb2760d50ec3ad19defdb494f587bab3ff7
Bipin <beepinb@gmail.com>
2018-08-17 18:18:42 +0545

    Login View Added

--- EnterInsideShooping/Controllers/LoginController.cs
+++ EnterInsideShooping/Controllers/LoginController.cs
@@ -11,7 +11,7 @@ namespace EnterInsideShooping.Controllers
         // GET: Login
         public ActionResult Index()
         {
-            return Content("This is login page");
+            return View();
         }
         public ActionResult Create()
         {
